95acbbe50bf04613b26766cdd1f10949ba02132c
nickstershovel <116566824+nickstershovel@users.noreply.github.com>
2022-12-22 16:57:12 +0100

    Exercise 32

---
+++ es-32/exercise-4.js
@@ -0,0 +1,10 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25,
+};
+const filteredPerson = {id: person.id, age: person.age}
+const json = JSON.stringify(filteredPerson);
+
+console.log(json); // Should return: { id: 1, age: 25 }
