8c33677b9766ca565b4ff2b6dae6bc3ed05b9110
nickstershovel <116566824+nickstershovel@users.noreply.github.com>
2023-01-27 21:23:12 +0100

    Props-1

--- Components-3/exercise.js
+++ Components-3/exercise.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Hello from "../Components-2/exercise"
+import Welcome from "../Props-1/exercise"
 class App extends React.Component {
     render() {
         <div>
             <Hello></Hello>
+            <Welcome></Welcome>
         </div>
     }
 }
---
+++ Props-1/exercise.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Welcome extends React.Component {
+    render(){
+        <p>Hello, {this.props.name}!</p>
+    }
+}
+
+export default Welcome;
