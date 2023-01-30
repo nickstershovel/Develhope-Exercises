89a29987cae356c518fdbd61875a2cf8b84075f1
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-01-30 21:51:16 +0100

    Conditional Rendering 2

--- my-app/src/props.js
+++ my-app/src/props.js
@@ -10,7 +10,7 @@ class Welcome extends React.Component {
         return(
         <div>
         <p>Hello, {this.props.name}!</p>
-        {this.props.age > 18 && <Age age={this.props.age} />}
+        {this.props.age && <Age age={this.props.age} />}
         </div>
         )
     }
