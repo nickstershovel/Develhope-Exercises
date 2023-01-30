6228b76a12e3aa68beb7112978862a75f9cd51a9
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-01-30 14:23:05 +0100

    Props 3

--- Props-1/exercise.js
+++ Props-1/exercise.js
@@ -2,7 +2,10 @@ import React from "react";
 
 class Welcome extends React.Component {
     render(){
+        <div>
         <p>Hello, {this.props.name}!</p>
+        <p>Your age is {this.props.age}</p>
+        </div>
     }
 }
 Welcome.defaultProps = {
