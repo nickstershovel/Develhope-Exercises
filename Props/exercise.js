fbd251efbcc0244b9b822203950af5f665b1e930
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-01-30 18:43:58 +0100

    Props-5

--- Props/exercise.js
+++ Props/exercise.js
@@ -1,10 +1,15 @@
 import React from "react";
+class Age extends React.Component {
+    render() {
+        return <p>Your age is {this.props.age}</p>;
+    }
+}
 
 class Welcome extends React.Component {
     render(){
         <div>
         <p>Hello, {this.props.name}!</p>
-        <p>Your age is {this.props.age}</p>
+        <Age age={this.props.age} />
         </div>
     }
 }
