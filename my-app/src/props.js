de1be98206bc12f66c29161883dca38e3e684d60
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-01-30 21:56:38 +0100

    Conditional Rendering 5

--- my-app/src/props.js
+++ my-app/src/props.js
@@ -1,7 +1,11 @@
 import React from "react";
 class Age extends React.Component {
     render() {
-        return <p>Your age is {this.props.age}</p>;
+        return this.props.age > 18 ? (
+            <p>Your age is {this.props.age}</p>
+        ) : (
+            <p>You are very young!</p>
+        );
     }
 }
 
