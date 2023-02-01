9069c38b64fc0ae5955b0d53cd847742dcbbf11b
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-01 19:50:34 +0100

    State 04

--- my-app/src/state.js
+++ my-app/src/state.js
@@ -31,7 +31,43 @@ import React from 'react';
 // }
 
 
-//STATE 03-Exercise
+// STATE 03-Exercise
+// class Counter extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             count: this.props.initialValue
+//         };
+//     }
+
+//     componentDidMount() {
+//         this.intervalId = setInterval(() => {
+//             this.setState(prevState => ({
+//                 count: prevState.count + this.props.incrementAmount
+//             }));
+//         }, this.props.incrementInterval);
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.intervalId);
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Count: {this.state.count}</h1>
+//             </div>
+//         );
+//     }
+// }
+//STATE 04-Exercise
+class CounterDisplay extends React.Component {
+    render (){
+        return (
+        <h1>Count: {this.props.count}</h1>
+        )
+    }
+}
 class Counter extends React.Component {
     constructor(props) {
         super(props);
@@ -55,7 +91,7 @@ class Counter extends React.Component {
     render() {
         return (
             <div>
-                <h1>Count: {this.state.count}</h1>
+                <CounterDisplay count={this.state.count}/>
             </div>
         );
     }
