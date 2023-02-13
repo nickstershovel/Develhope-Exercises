03f3f4b02a275f4331988d62324aea2c0eeb1a4a
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-13 15:20:58 +0100

    Component Compositions  01

---
+++ my-app/src/compositions.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Container = ({ children }) => (
+    <div style={{ background: "white", border: "1px solid red" }}>
+        {children}
+    </div>
+);
+
+export default Container;
