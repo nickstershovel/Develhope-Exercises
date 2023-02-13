683631de019808d9d0d58913db86db1ec8f1bca2
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-13 15:22:36 +0100

    Component Composition 2

--- my-app/src/compositions.js
+++ my-app/src/compositions.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Container = ({ children }) => (
-    <div style={{ background: "white", border: "1px solid red" }}>
+const Container = ({ children, title }) => (
+    <div style={{ background: "white", border: "1px solid red", padding: "10px" }}>
+        <h3>{title}</h3>
         {children}
     </div>
 );
