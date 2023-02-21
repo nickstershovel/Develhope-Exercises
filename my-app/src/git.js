c3c96a4b87ff6a1d5fed322ae79518bd9f674b83
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-22 00:20:13 +0100

    Custom Hooks 04

--- my-app/src/git.js
+++ my-app/src/git.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+
 function GithubUser(props) {
     const [userData, setUserData] = useState(null);
 
--- my-app/src/useGitHubUser.js
+++ my-app/src/useGitHubUser.js
@@ -2,15 +2,25 @@ import { useState, useEffect } from "react";
 
 function useGithubUser(username) {
     const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    useEffect(() => {
-        async function fetchUserData() {
+    const fetchUserData = async () => {
+        try {
+            setLoading(true);
             const response = await fetch(`https://api.github.com/users/${username}`);
             const data = await response.json();
             setUserData(data);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            setError(error);
         }
+    };
+
+    useEffect(() => {
         fetchUserData();
     }, [username]);
 
-    return userData;
+    return { userData, loading, error, fetchUserData };
 }
