4c7637f4e9242914c0560e216ec52791f78a0001
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-21 22:56:23 +0100

    Function Components 01

--- my-app/src/props.js
+++ my-app/src/props.js
@@ -1,29 +1,48 @@
+// import React from "react";
+// class Age extends React.Component {
+//     render() {
+//         return this.props.age > 18 ? (
+//             <p>Your age is {this.props.age}</p>
+//         ) : (
+//             <p>You are very young!</p>
+//         );
+//     }
+// }
+
+// class Welcome extends React.Component {
+//     render(){
+//         return(
+//         <div>
+//         <p>Hello, {this.props.name}!</p>
+//         {this.props.age > 18 &&
+//                 this.props.age < 65 &&
+//                 this.props.name === "John" && (
+//                     <Age age={this.props.age} />
+//                 )}
+//         </div>
+//         )
+//     }
+// }
+// Welcome.defaultProps = {
+//     name: "Guest"
+// };
+// export default Welcome;
 import React from "react";
-class Age extends React.Component {
-    render() {
-        return this.props.age > 18 ? (
-            <p>Your age is {this.props.age}</p>
-        ) : (
-            <p>You are very young!</p>
-        );
-    }
+
+function Welcome(props) {
+    const { name = "Guest", age } = props;
+
+    return (
+        <div>
+            <p>Hello, {name}!</p>
+            {age > 18 && age < 65 && name === "John" && (
+                <p>Your age is {age}</p>
+            )}
+            {!(age > 18 && age < 65 && name === "John") && (
+                <p>You are very young!</p>
+            )}
+        </div>
+    );
 }
 
-class Welcome extends React.Component {
-    render(){
-        return(
-        <div>
-        <p>Hello, {this.props.name}!</p>
-        {this.props.age > 18 &&
-                this.props.age < 65 &&
-                this.props.name === "John" && (
-                    <Age age={this.props.age} />
-                )}
-        </div>
-        )
-    }
-}
-Welcome.defaultProps = {
-    name: "Guest"
-};
 export default Welcome;
