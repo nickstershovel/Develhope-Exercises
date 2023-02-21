55bfb30cd496aca4051349b7b69ec5f3aefaafa4
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-21 23:28:51 +0100

    UseState 01

--- my-app/src/events-1.js
+++ my-app/src/events-1.js
@@ -1,26 +1,18 @@
-import React from "react";
-//EVENTS 01-Exerecise
-class ClickCounter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: this.props.initialValue ?? 0
-        };
-    }
+import React, { useState } from "react";
 
-    handleClick = () => {
-        this.setState(prevState => ({ count: prevState.count + 1 }));
+function ClickCounter(props) {
+    const [count, setCount] = useState(props.initialValue ?? 0);
+
+    const handleClick = () => {
+        setCount(prevCount => prevCount + 1);
     };
 
-    render() {
-        return (
-            <div>
-                <p>Current count: {this.state.count}</p>
-                <button onClick={this.handleClick}>Click me</button>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <p>Current count: {count}</p>
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    );
 }
-
 
 export default ClickCounter;
