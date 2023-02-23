baeea2b2d3e86ada62457134de034e5ca6a3263c
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-23 15:44:11 +0100

    UseCallBACK

--- my-app/src/useCounterHook.js
+++ my-app/src/useCounterHook.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 function useCounter(initialValue = 0) {
     const [count, setCount] = useState(initialValue);
 
-    const increment = () => {
+    const increment = useCallback(() => {
         setCount(prevCount => prevCount + 1);
-    };
+    }, []);
 
-    const decrement = () => {
+    const decrement = useCallback(() => {
         setCount(prevCount => prevCount - 1);
-    };
+    }, []);
 
-    const reset = () => {
+    const reset = useCallback(() => {
         setCount(initialValue);
-    };
+    }, [initialValue]);
 
     return [count, increment, decrement, reset];
 }
