0986a0b670c5ee7d2c0107662531750ddd9b6750
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-26 15:16:15 +0100

    SWR 2

--- my-app/src/useGitHubUser.js
+++ my-app/src/useGitHubUser.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from "react";
 import useSWR from "swr";
 
 function useGithubUser(username) {
-    const { data, error } = useSWR(`https://api.github.com/users/${username}`);
+    if (!username) {
+        return {
+            userData: null,
+            loading: false,
+            error: null,
+            fetchUserData: () => { },
+        };
+    }
 
-    return {
-        userData: data,
-        loading: !error && !data,
-        error,
+    const fetcher = async (url) => {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
     };
+
+    const { data: userData, error, isValidating: loading } = useSWR(
+        `https://api.github.com/users/${username}`,
+        fetcher
+    );
+
+    return { userData, loading, error, fetchUserData: () => { } };
 }
+
+export default useGithubUser;
