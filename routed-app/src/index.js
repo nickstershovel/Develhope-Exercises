fced7073fefbf86b3948aac4d979bba7236d5f78
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-26 15:00:45 +0100

    React Router 04

--- routed-app/src/index.js
+++ routed-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Welcome from "./props";
 import Counter from "./state";
 import GithubUser from "./git";
@@ -14,20 +14,36 @@ function ShowGithubUser() {
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Welcome name="John" />} />
-                <Route
-                    path="/counter"
-                    element={
-                        <Counter
-                            initialValue={3}
-                            incrementAmount={0.89}
-                            incrementInterval={1000}
-                        />
-                    }
-                />
-                <Route path="/users/:username" element={<ShowGithubUser />} />
-            </Routes>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Welcome</Link>
+                        </li>
+                        <li>
+                            <Link to="/counter">Counter</Link>
+                        </li>
+                        <li>
+                            <Link to="/users/nickstershovel">nickstershovel</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<Welcome name="John" />} />
+                    <Route
+                        path="/counter"
+                        element={
+                            <Counter
+                                initialValue={3}
+                                incrementAmount={0.89}
+                                incrementInterval={1000}
+                            />
+                        }
+                    />
+                    <Route path="/users/:username" element={<ShowGithubUser />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 }
