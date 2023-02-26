0307f7ee3e9cd5afc4a99939649a70310d5acd48
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-02-26 14:58:16 +0100

    React Router 03

--- routed-app/src/index.js
+++ routed-app/src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./props";
 import Counter from "./state";
+import GithubUser from "./git";
+import { useParams } from "react-router-dom";
+
+function ShowGithubUser() {
+    const { username } = useParams();
+    return <GithubUser username={username} />;
+}
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Welcome name="John" />} />
-                <Route path="/counter" element={<Counter initialValue={3} incrementAmount={0.89} incrementInterval={1000}/>}/>
+                <Route
+                    path="/counter"
+                    element={
+                        <Counter
+                            initialValue={3}
+                            incrementAmount={0.89}
+                            incrementInterval={1000}
+                        />
+                    }
+                />
+                <Route path="/users/:username" element={<ShowGithubUser />} />
             </Routes>
         </BrowserRouter>
     );
@@ -17,3 +34,4 @@ function App() {
 
 ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
+
