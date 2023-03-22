2e8f2bd29a2cf132a756cb91e925cee63eb9d6eb
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-03-23 00:00:32 +0100

    Clean up code.

---
+++ prisma/migrations/20230322225921_migration/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "Planet" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "diameter" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Planet_pkey" PRIMARY KEY ("id")
+);
