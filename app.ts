85946bd0c46c7148605b10c0560506281a9a3cb2
ariaworld <116566824+nickstershovel@users.noreply.github.com>
2023-03-23 13:23:58 +0100

    Fix errors.

---
+++ app.ts
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello, world!" });
+});
+
+
+app.post('/planets', async (req, res) => {
+    try {
+        const planet  = req.body;
+        const newPlanet = await prisma.planet.create({
+            data: planet
+        });
+        res.status(201).json(newPlanet);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
--- prisma/migrations/20230323122257_init/migration.sql
+++ prisma/migrations/20230323122257_init/migration.sql
@@ -1,18 +1,9 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Post";
-
 -- CreateTable
 CREATE TABLE "Planet" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
-    "diameter" DOUBLE PRECISION NOT NULL,
+    "diameter" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
