5eb8ce8421b52a25c6a7196c0a101eb6318d304a
nickstershovel <116566824+nickstershovel@users.noreply.github.com>
2023-04-06 11:34:04 +0200

    Add: Documentation to "planets.ts, app.ts"

--- app.ts
+++ app.ts
@@ -1,3 +1,10 @@
+/**
+
+This file sets up the Express application and exports it as the default export.
+It includes middleware for parsing JSON request bodies, enabling CORS, and handling validation errors.
+It also includes middleware for initializing and using sessions and Passport authentication.
+The app uses routes for handling requests related to planets and authentication.
+*/
 import { Express } from "express";
 import cors from "cors";
 
@@ -8,10 +15,11 @@ import { passport } from "./lib/middleware/passport";
 import planetsRoutes from "./routes/planets";
 
 import authRoutes from "./routes/auth";
+
 // Set CORS options
 const corsOptions = {
-    origin: "http://localhost:8080",
-    credentials: true,
+origin: "http://localhost:8080",
+credentials: true,
 };
 
 // Initialize express application
@@ -20,13 +28,17 @@ const app = express();
 
 // Get port from environment variable
 const port = process.env.PORT;
+
+// Use middleware for initializing and using sessions and Passport authentication
 app.use(initSessionMiddleware());
 app.use(passport.initialize());
 app.use(passport.session());
+
 // Set up middleware
 app.use(express.json()); // Parse JSON request body
 app.use(cors(corsOptions)); // Enable CORS
 
+// Use routes for handling requests related to planets and authentication
 app.use("/planets", planetsRoutes);
 app.use("/auth", authRoutes);
 
--- routes/planets.ts
+++ routes/planets.ts
@@ -1,106 +1,107 @@
-import express, { Router } from "express";
-
 // Import required modules
-import prisma from "../lib/client";
-import cors from "cors";
-import { validate, PlanetData, planetSchema } from "../lib/validation";
-
-import { checkAuthorization } from "../lib/middleware/passport";
-
-import { initMulterMiddleware } from "../lib/middleware/multer";
+import express, { Router } from "express"; // Importing the express module and the Router class from the express package to use them for creating a router object and defining routes in our application.
+import prisma from "../lib/client"; // Import the Prisma client for database access
+import cors from "cors"; // Import the CORS middleware for handling cross-origin requests
+import { validate, PlanetData, planetSchema } from "../lib/validation"; // Import validation functions and schema for validating planet data
+import { checkAuthorization } from "../lib/middleware/passport"; // Import middleware function for checking user authentication
+import { initMulterMiddleware } from "../lib/middleware/multer"; // Import middleware function for handling file uploads
 
 // Initialize multer middleware for handling file uploads
 const upload = initMulterMiddleware();
 
-const router = Router();
+const router = Router(); // Create a new router instance
 
 // Get all planets
 router.get("/", async (req, res) => {
     try {
-        const resources = await prisma.planet.findMany();
-        res.json(resources);
+        const resources = await prisma.planet.findMany(); // Use Prisma client to retrieve all planets from the database
+        res.json(resources); // Return the retrieved planets as JSON
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message }); // Return an error message if there was an error retrieving the planets
     }
 });
 
 // Create a new planet
 router.post(
     "/",
-    checkAuthorization,
-    validate({ body: planetSchema }),
+    checkAuthorization, // Use the checkAuthorization middleware to ensure user is authenticated
+    validate({ body: planetSchema }), // Use the validate middleware to validate the planet data against the planetSchema
     async (req, res) => {
-        const planetData: PlanetData = req.body;
-        const username = req.user.username
+        const planetData: PlanetData = req.body; // Extract the validated planet data from the request body
+        const username = req.user.username; // Extract the authenticated user's username from the request object
 
         const planet = await prisma.planet.create({
+            // Use the Prisma client to create a new planet in the database
             data: {
                 ...planetData,
-                createdBy: username,
-                updatedBy: username,
-            }
+                createdBy: username, // Set the createdBy field to the authenticated user's username
+                updatedBy: username, // Set the updatedBy field to the authenticated user's username
+            },
         });
-        res.status(201).json(planet);
+        res.status(201).json(planet); // Return the newly created planet as JSON with a 201 Created status code
     }
 );
 
 // Get a planet by ID
-router.get("/:id(\\d+)", async (req, res, next) => {
-    const planetId = parseInt(req.params.id);
-    const planet = await prisma.planet.findUnique({ where: { id: planetId } });
+router.get("/:id(d+)", async (req, res, next) => {
+    const planetId = parseInt(req.params.id); // Extract the planet ID from the URL parameters
+    const planet = await prisma.planet.findUnique({ where: { id: planetId } }); // Use the Prisma client to retrieve the planet with the specified ID from the database
 
     if (!planet) {
+        // If the planet was not found, return a 404 Not Found error
         res.status(404);
         return next(`Cannot GET /planets/${planetId}`);
     }
-    res.json(planet);
+    res.json(planet); // Return the retrieved planet as JSON
 });
 
 // Update a planet by ID
 router.put(
-    "/:id(\\d+)",
-    checkAuthorization,
-    validate({ body: planetSchema }),
+    "/:id(d+)",
+    checkAuthorization, // Use the checkAuthorization middleware to ensure user is authenticated
+    validate({ body: planetSchema }), // Use the validate middleware to validate the planet data against the planetSchema
     async (req, res, next) => {
-        const planetId = parseInt(req.params.id);
-        const planetData = req.body;
-        const username = req.user?.username as string;
+        const planetId = parseInt(req.params.id); // Extract the planet ID from the URL parameters
+        const planetData = req.body; // Extract the validated planet data from the request body
+        const username = req.user?.username as string; // Extract the authenticated user's username from the request object
 
         try {
             const updatedPlanet = await prisma.planet.update({
+                // Use the Prisma client to update the planet with the specified ID in the database
                 where: { id: planetId },
                 data: {
                     ...planetData,
-                    updatedBy: username
-                }
+                    updatedBy: username, // Set the updatedBy field to the authenticated user's username
+                },
             });
-            res.json(updatedPlanet);
+            res.json(updatedPlanet); // Return the updated planet as JSON
         } catch (error) {
-            res.status(404);
+            res.status(404); // If the planet was not found, return a 404 Not Found error
             next(`Cannot PUT /planets/${planetId}`);
         }
     }
 );
 
 // Delete a planet by ID
-router.delete("/:id(\\d+)", checkAuthorization, async (req, res, next) => {
-    const planetId = parseInt(req.params.id);
+router.delete("/:id(d+)", checkAuthorization, async (req, res, next) => {
+    const planetId = parseInt(req.params.id); // Extract the planet ID from the URL parameters
 
     try {
         await prisma.planet.delete({
+            // Use the Prisma client to delete the planet with the specified ID from the database
             where: { id: planetId },
         });
-        res.status(204).end();
+        res.status(204).end(); // Return a 204 No Content status code to indicate successful deletion
     } catch (error) {
-        res.status(404);
+        res.status(404); // If the planet was not found, return a 404 Not Found error
         next(`Cannot DELETE /planets/${planetId}`);
     }
 });
 
 // Define an endpoint to upload a photo for a planet with a specific ID
 router.post(
-    "/:id(\\d+)/photo", // URL pattern with regex to only accept numeric IDs
-    checkAuthorization,
+    "/:id(d+)/photo", // URL pattern with regex to only accept numeric IDs
+    checkAuthorization, // Use the checkAuthorization middleware to ensure user is authenticated
     upload.single("photo"), // Use the 'upload' middleware to handle file uploads
 
     // Handler function for the endpoint
@@ -123,7 +124,7 @@ router.post(
             });
             res.status(201).json(photoFilename); // Set response status code to 201 Created and return the photo filename as JSON
         } catch (error) {
-            res.status(404); // Set response status code to 404 Not Found
+            res.status(404); // If the planet was not found, return a 404 Not Found error
             next(`Cannot POST /planets/${planetId}/photo`); // Call the next middleware with an error message
         }
     }
@@ -132,4 +133,4 @@ router.post(
 // Serve uploaded photos at a separate URL endpoint
 router.use("/photos/", express.static("uploads"));
 
-export default router;
+export default router; // Export the router for use in other modules
